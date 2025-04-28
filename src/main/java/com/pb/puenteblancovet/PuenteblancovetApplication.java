7b65a65aa3f068b66ac5b0a11c4bd8e8cf33ae06
GustavoFernandez <bentevocks2@gmail.com>
2025-04-28 04:45:51 -0500

    init: Estructura base del sistema Puente Blanco

---
+++ src/main/java/com/pb/puenteblancovet/PuenteblancovetApplication.java
@@ -0,0 +1,13 @@
+package com.pb.puenteblancovet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuenteblancovetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuenteblancovetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pb/puenteblancovet/PuenteblancovetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pb.puenteblancovet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuenteblancovetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
